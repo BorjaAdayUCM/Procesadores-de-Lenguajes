389abae319e2637875177d8eee3829048b04281c
Diego De Miguel López <ddemiguellopez@gmail.com>
2021-03-08 22:01:52 +0100

    fffddd

---
+++ PracticaPL2/src/ALexOperations.java
@@ -0,0 +1,248 @@
+
+public class ALexOperations {
+	private AnalizadorLexicoTiny alex;
+
+	public ALexOperations(AnalizadorLexicoTiny alex) {
+		this.alex = alex;
+	}
+
+	public UnidadLexica unidadId() {
+		return new UnidadLexicaMultivaluada(alex.fila(), alex.columna(), ClaseLexica.IDEN, alex.lexema());
+	}
+
+	public UnidadLexica unidadNumEnt() {
+		return new UnidadLexicaMultivaluada(alex.fila(), alex.columna(), ClaseLexica.NUMENT, alex.lexema());
+	}
+
+	public UnidadLexica unidadNumReal() {
+		return new UnidadLexicaMultivaluada(alex.fila(), alex.columna(), ClaseLexica.NUMREAL, alex.lexema());
+	}
+
+	public UnidadLexica unidadCadena() {
+		return new UnidadLexicaMultivaluada(alex.fila(), alex.columna(), ClaseLexica.CADENA, alex.lexema());
+	}
+
+	public UnidadLexica unidadMas() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.MAS);
+	}
+
+	public UnidadLexica unidadMenos() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.MENOS);
+	}
+
+	public UnidadLexica unidadPor() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.POR);
+	}
+
+	public UnidadLexica unidadDiv() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.DIV);
+	}
+
+	public UnidadLexica unidadSep() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.SEP);
+	}
+
+	public UnidadLexica unidadMod() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.MOD);
+	}
+
+	public UnidadLexica unidadMenor() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.MENOR);
+	}
+
+	public UnidadLexica unidadMayor() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.MAYOR);
+	}
+
+	public UnidadLexica unidadMayorigual() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.MAYORIGUAL);
+	}
+
+	public UnidadLexica unidadMenorigual() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.MENORIGUAL);
+	}
+
+	public UnidadLexica unidadIgual() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.IGUAL);
+	}
+
+	public UnidadLexica unidadIgualdad() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.IGUALDAD);
+	}
+
+	public UnidadLexica unidadDistitnto() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.DISTINTO);
+	}
+
+	public UnidadLexica unidadPAper() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.PAPER);
+	}
+
+	public UnidadLexica unidadPCierre() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.PCIERRE);
+	}
+
+	public UnidadLexica unidadPyC() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.PYC);
+	}
+
+	public UnidadLexica unidadCoraper() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.CORAPER);
+	}
+
+	public UnidadLexica unidadCorcierre() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.CORCIERRE);
+	}
+
+	public UnidadLexica unidadLlaveaper() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.LLAVEAPER);
+	}
+
+	public UnidadLexica unidadLlavecierre() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.LLAVECIERRE);
+	}
+
+	public UnidadLexica unidadPunto() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.PUNTO);
+	}
+
+	public UnidadLexica unidadComa() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.COMA);
+	}
+
+	public UnidadLexica unidadFlecha() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.FLECHA);
+	}
+
+	public UnidadLexica unidadAmpersand() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.AMPERSAND);
+	}
+
+	public UnidadLexica unidadInt() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.INT);
+	}
+
+	public UnidadLexica unidadReal() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.REAL);
+	}
+
+	public UnidadLexica unidadBool() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.BOOL);
+	}
+
+	public UnidadLexica unidadString() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.STRING);
+	}
+
+	public UnidadLexica unidadAnd() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.AND);
+	}
+
+	public UnidadLexica unidadOr() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.OR);
+	}
+
+	public UnidadLexica unidadNot() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.NOT);
+	}
+
+	public UnidadLexica unidadNull() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.NULL);
+	}
+
+	public UnidadLexica unidadTrue() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.TRUE);
+	}
+
+	public UnidadLexica unidadFalse() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.FALSE);
+	}
+
+	public UnidadLexica unidadProc() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.PROC);
+	}
+
+	public UnidadLexica unidadIf() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.IF);
+	}
+
+	public UnidadLexica unidadThen() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.THEN);
+	}
+
+	public UnidadLexica unidadElse() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.ELSE);
+	}
+
+	public UnidadLexica unidadEndIf() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.ENDIF);
+	}
+
+	public UnidadLexica unidadWhile() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.WHILE);
+	}
+
+	public UnidadLexica unidadDo() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.DO);
+	}
+
+	public UnidadLexica unidadEndWhile() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.ENDWHILE);
+	}
+
+	public UnidadLexica unidadCall() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.CALL);
+	}
+
+	public UnidadLexica unidadRecord() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.RECORD);
+	}
+
+	public UnidadLexica unidadArray() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.ARRAY);
+	}
+
+	public UnidadLexica unidadOf() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.OF);
+	}
+
+	public UnidadLexica unidadPointer() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.POINTER);
+	}
+
+	public UnidadLexica unidadNew() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.NEW);
+	}
+
+	public UnidadLexica unidadDelete() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.DELETE);
+	}
+
+	public UnidadLexica unidadRead() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.READ);
+	}
+
+	public UnidadLexica unidadWrite() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.WRITE);
+	}
+
+	public UnidadLexica unidadNl() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.NL);
+	}
+
+	public UnidadLexica unidadVar() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.VAR);
+	}
+
+	public UnidadLexica unidadType() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.TYPE);
+	}
+
+	public UnidadLexica unidadEof() {
+		return new UnidadLexicaUnivaluada(alex.fila(), alex.columna(), ClaseLexica.EOF);
+	}
+
+	public void error() {
+		System.err.println("***" + alex.fila() + " Caracter inexperado: " + alex.lexema());
+	}
+}
--- PracticaPL2/src/Main.java
+++ PracticaPL2/src/Main.java
@@ -1,9 +1,20 @@
 
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+   public static void main(String[] args) throws FileNotFoundException, IOException {
+     Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+     AnalizadorLexicoTiny al = new AnalizadorLexicoTiny(input);
+     UnidadLexica unidad;
+     do {
+       unidad = al.yylex();
+       System.out.println(unidad);
+     }
+     while (unidad.clase() != ClaseLexica.EOF);
+    }        
+} 
